aa80ced768496e25d533ba48d8fb03ffad672744
Mathieu Lecarme <mathieu@garambrogne.net>
2012-05-01 23:34:27 +0200

    Handling _, < and some TODOs.

--- src/aphone_py/__init__.py
+++ src/aphone_py/__init__.py
@@ -12,14 +12,26 @@ class Phonet(object):
             txt_size += 1
         fun = """def filter_%i(txt, out):
 """ % self.cpt
+        #  TODO handles clause as list, and joining with "and"
+        #  TODO $ => len == size
+        #  TODO handling ^ out == "" ? or a flag?
         if rule['alternates'] != u"":
             fun += """    if len(txt) >= %i and txt[:%i] == u'%s' and txt[%i] in u'%s':
 """ % (txt_size, len(rule['text']), rule['text'], len(rule['text']), rule['alternates'])
         else:
             fun += """    if len(txt) >= %i and txt[:%i] == u'%s':
 """ % (txt_size, len(rule['text']), rule['text'])
-        fun += """        return txt[%i:], out += u"%s"
-""" % (len(rule['text']) - rule['minus'], rule['replace'])
+
+        if rule['replace'] == u"_":
+            replace = u""
+        else:
+            replace = rule['replace']
+        if rule['again']:
+            fun += """        return u"%s" + txt[%i:], out
+""" % (replace, len(rule['text']) - rule['minus'])
+        else:
+            fun += """        return txt[%i:], out += u"%s"
+""" % (len(rule['text']) - rule['minus'], replace)
         return fun
 
 
